daeb14e85eddc8eebbd976ce269631734a83a2b1
Lucas Gabriel <165740753+Lucasalmeeida@users.noreply.github.com>
2025-05-15 21:16:17 -0300

    att

---
+++ script.js
@@ -0,0 +1,8 @@
+let botao = document.querySelector("botao")
+botao.style.background="blue";
+
+botao.addEventListener("mouseover",trocaverde)
+
+function trocaverde(){
+    botao.style.background="green";
+}
